564ffce3e45bee251235d19dc207a5ef5273d314
OnlyMathias7 <mathias.saldanha@gmail.com>
2026-01-31 15:34:01 +0100

    Reorganisation: tous les fichiers dans le dossier TP1

---
+++ TP1_Vision/funct_conv_dim.m
@@ -0,0 +1,7 @@
+function [M] = funct_conv_dim(MCH)
+    n=size(MCH,1)
+    M = zeros(n-1, size(MCH, 2)); % Initialize M with appropriate dimensions
+    for i=1:n-1
+        M(i,:) = MCH(i,:)./MCH(n,:);
+    end
+end
