164f6993feb3dfcc55148ec080e1a292f96e634e
OnlyMathias7 <mathias.saldanha@gmail.com>
2026-02-12 13:12:15 +0100

    Modification de la fonction affiche_image

--- TP2_Vision/affiche_image.m
+++ TP2_Vision/affiche_image.m
@@ -3,10 +3,13 @@ function [] = affiche_image(lots_images)
     figure;
     for j=1:N
         for i=1:M
-            img = uint8(lots_images{i});
-            subplot(j,M,i);
-            hold on
-            imshow(img);   
+                img = uint8(lots_images{i}{1});
+                subplot(j,M,i);
+                hold on
+                imshow(img); 
+                if(size(lots_images{i}, 2)~=1)
+                    title(lots_images{i}{2}); 
+                end    
         end
     end
 end 
--- TP2_Vision/tp2.m
+++ TP2_Vision/tp2.m
@@ -5,12 +5,12 @@ clc, clear, close all,
 img = double(imread('photophore.tif'));
 n = size(img);
 alpha = 05;
-img_bruite = img + 100*randn(size(img));
+img_bruite = img + 150*randn(size(img));
 img_est = algo_iter_Gauss_Seidel(img_bruite,alpha, 1000);
 flt = ones(5);
 flt = flt/sum(sum(flt));
 img_filtre = conv2(img_est,flt,'same');
 
 
-lots_image = {img,img_bruite, img_est, img_filtre};
+lots_image = {{img, "Image de Base"},{img_bruite, "Image bruité"}, {img_est, "Image restaurée"}, {img_filtre, "Image filtré"}};
 affiche_image(lots_image);
